888130a0e99b6eb1ca9ddef1ff4b71b515d5754f
MarcusKirkegaard <50557123+MarcusKirkegaard@users.noreply.github.com>
2019-06-04 09:00:00 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,26 @@
+// JavaScript Document
+//Funktion til at vise/gemme undermenupunkter
+	$("#emner").click(function() {
+		$(".main-nav-emne1").toggle();
+	});// 
+
+// JavaScript Document
+//Funktion til at vise/gemme undermenupunkter
+	$("#emner").click(function() {
+		$(".main-nav-emne2").toggle();
+	});// 
+
+
+// JavaScript Document
+//Funktion til at vise/gemme undermenupunkter
+	$("#emner").click(function() {
+		$(".main-nav-emne3").toggle();
+	});// 
+
+
+// JavaScript Document
+//Funktion til at vise/gemme undermenupunkter
+	$("#emner").click(function() {
+		$(".main-nav-emne4").toggle();
+	});// 
+
